acb78d51a3704ec3b58a6b226728ec4c7caccb85
Vanessa Nava <vnava26@gmail.com>
2016-08-25 21:16:31 -0700

    added objects

--- objects/objects.js
+++ objects/objects.js
@@ -6,15 +6,25 @@ var twitter = {
 };
 
 // 1. Dot notation:
+stockPrice.number = 18;
 
 // 2. Bracket notation:
+ceo['name'] = 'Jack Dorsey';
 
 // 3. Practice on your own:
 var twittersAddress = "1355 Market St #900, San Francisco, CA 94103";
+twitter['address'] = '1355 Market St #900, San Francisco, CA 94103';
+// I used bracket notation
 
 // 4. Array:
+twitter = ['tweet1', 'tweet2'];
 
 // 5. Loop through array:
+var loop = function(tweet) {
+	var print = function(tweet) {
+		console.log(tweet);
+	}
+};
 
 // 5. Invoke countCharacters:
 
