9614e6df1538525d73a9728659d88a9db1989c67
Vanessa Nava <vnava26@gmail.com>
2016-08-25 21:13:25 -0700

    added functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,9 +1,17 @@
 // Invoke each function you write with an example and console.log the result
 
 // multiply
+var multiply = function (a, b){
+      return (a * b);
+  };
+      multiply (2 * 1);
+      console.log(two)
 
 // whatForLunch
+var whatForLunch = function (name, food) {
+return name + 'had' + food + 'for lunch today';
 
+};
 // isLargerThan100
 
 // anyEvens
